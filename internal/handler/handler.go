cc46c5f2633f0c8fa2e04e197ab8045d49f58cb7
ixoja <khanbatyj@gmail.com>
2019-05-16 18:45:21 +0300

    gin swagger init

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,1 +1,21 @@
 package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/ixoja/shorten/internal/restapi/operations"
+	"github.com/mikkeloscar/gin-swagger/api"
+)
+
+type Service struct {
+
+}
+
+func (s *Service) Healthy() bool {
+	return true
+}
+func (s *Service) GetHash(ctx *gin.Context, params *operations.GetHashParams) *api.Response {
+	return &api.Response{}
+}
+func (s *Service) PostShorten(ctx *gin.Context, params *operations.PostShortenParams) *api.Response {
+	return &api.Response{}
+}
